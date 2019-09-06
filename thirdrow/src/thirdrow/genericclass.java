bdee5ca7dedab302f518f7697fe01e7513f2da6a
zechariah.roberson <zechariah.roberson@MXL3250J0F.DRMAIN.local>
2019-09-06 14:06:52 -0400

    Init Commit

---
+++ thirdrow/src/thirdrow/genericclass.java
@@ -0,0 +1,11 @@
+package thirdrow;
+
+public class genericclass {
+
+	public static void main(String[] args) {
+		System.out.println("Please input your name in a line below");
+		System.out.println("This Line is from Zech");
+
+	}
+
+}
