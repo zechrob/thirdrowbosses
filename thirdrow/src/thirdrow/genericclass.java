5c122c89f88f5c9412f77666b7b7e99002360630
mario mcclelland <loloiram@gmail.com>
2019-09-06 14:21:04 -0400

    mariosss

--- thirdrow/src/thirdrow/genericclass.java
+++ thirdrow/src/thirdrow/genericclass.java
@@ -7,6 +7,10 @@ public class genericclass {
 		System.out.println("This Line is from Zech");
 		System.out.println("This is Reid's Line");
 		System.out.println("This line is from Mario!");
+		
+		
+		
+		
 	}
 
 }
