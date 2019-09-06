c653e1644e5c017d7cee7066852c47b04375e77a
reid.britt <reid.britt@2UA3311GDG.DRMAIN.local>
2019-09-06 14:12:45 -0400

    Reid's Branch

--- thirdrow/src/thirdrow/genericclass.java
+++ thirdrow/src/thirdrow/genericclass.java
@@ -5,7 +5,7 @@ public class genericclass {
 	public static void main(String[] args) {
 		System.out.println("Please input your name in a line below");
 		System.out.println("This Line is from Zech");
-		System.out.println("I am Reid");
+		System.out.println("This is Reid's Line");
 	}
 
 }
